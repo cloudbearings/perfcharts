9ea800e8954102bf82e48f512d723a8c92299b33
Rayson Zhu <vfreex@gmail.com>
2015-07-22 16:15:06 +0800

    use CSVParser to resolve NMON files

--- src/chartgeneration-perftest-parser/java/chartgeneration/parser/NMONParser.java
+++ src/chartgeneration-perftest-parser/java/chartgeneration/parser/NMONParser.java
@@ -1,7 +1,9 @@
 package chartgeneration.parser;
 
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 
 import java.io.*;
 import java.text.ParseException;
@@ -17,8 +19,7 @@ public class NMONParser implements DataParser {
     public void parse(InputStream in, OutputStream out) throws IOException,
             ParseException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        String lineStr;
-        List<String[]> lines = new LinkedList<String[]>();
+        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
         Map<Integer, Long> timeTable = new HashMap<Integer, Long>(16000);
         //Map<String, String> metaInfo = new HashMap<String, String>(30);
         // TimeZone utcZone = TimeZone.getTimeZone("UTC");
@@ -26,39 +27,45 @@ public class NMONParser implements DataParser {
         // timeFormat.setTimeZone(utcZone);
 
         final Map<String, Integer> diskColumnMap = new HashMap<String, Integer>(4);
-        //Pattern diskPattern = Pattern.compile("^[hsv]d[a-z]$");
 
         Date startTime = Settings.getInstance().getStartTime();
         Date endTime = Settings.getInstance().getEndTime();
-
-        while ((lineStr = reader.readLine()) != null) {
-            String[] extractedLine = lineStr.split(",");
-            if (extractedLine.length == 0)
+        List<CSVRecord> records = new ArrayList<CSVRecord>();
+        for (Iterator<CSVRecord> it = csvParser.iterator(); ; ) {
+            try {
+                if (!it.hasNext())
+                    break;
+                records.add(it.next());
+            } catch (Exception ex) {
+                System.err.println("WARNING: invalid data row skipped (" + ex.toString() + ")");
+            }
+        }
+        //List<CSVRecord> records = csvParser.getRecords();
+        for (CSVRecord rec : records) {
+            if (rec.size() == 0)
                 continue;
-            lines.add(extractedLine);
-            if (extractedLine[0].equals("ZZZZ") && extractedLine.length >= 4) {
-                int tsLabelValue = Integer.parseInt(extractedLine[1]
+            if (rec.get(0).equals("ZZZZ") && rec.size() >= 4) {
+                int tsLabelValue = Integer.parseInt(rec.get(1)
                         .substring(1));
-                Date date = timeFormat.parse(extractedLine[2] + " "
-                        + extractedLine[3]);
+                Date date = timeFormat.parse(rec.get(2) + " "
+                        + rec.get(3));
                 if (startTime != null && date.before(startTime)
                         || endTime != null && date.after(endTime))
                     continue;
                 timeTable.put(tsLabelValue, date.getTime());
-            } else if (extractedLine[0].startsWith("DISK")
-                    && extractedLine.length > 2
-                    && !extractedLine[1].startsWith("T")) {
-                for (int i = 2; i < extractedLine.length; ++i) {
+            } else if (rec.get(0).startsWith("DISK")
+                    && rec.size() > 2
+                    && !rec.get(1).startsWith("T")) {
+                for (int i = 2; i < rec.size(); ++i) {
                     //if (diskPattern.matcher(extractedLine[i]).matches()) {
                     // diskColumnMap.putIfAbsent(extractedLine[i], i);
-                    diskColumnMap.put(extractedLine[i], i);
+                    diskColumnMap.put(rec.get(i), i);
                     //}
                 }
             }/* else if (extractedLine[0].equals("AAA")
                     && extractedLine.length >= 3) {
 				metaInfo.put(extractedLine[1], extractedLine[2]);
 			}*/
-
         }
         in.close();
 
@@ -66,63 +73,63 @@ public class NMONParser implements DataParser {
 
         keyParserMap.put("CPU_ALL", new NMONItemParser() {
             @Override
-            public void parse(CSVPrinter writer, String[] line, Map<Integer, Long> timeTable) throws IOException {
-                Long ts = timeTable.get(Integer.parseInt(line[1].substring(1)));
+            public void parse(CSVPrinter writer, CSVRecord rec, Map<Integer, Long> timeTable) throws IOException {
+                Long ts = timeTable.get(Integer.parseInt(rec.get(1).substring(1)));
                 if (ts == null)
                     return;
                 writer.printRecord("CPU",
                         ts.toString(),
-                        Float.toString(Float.parseFloat(line[2])),
-                        Float.toString(Float.parseFloat(line[3])),
-                        Float.toString(Float.parseFloat(line[4])),
-                        String.format("%.1f", 100.0f - Float.parseFloat(line[5])),
-                        Integer.toString(Integer.parseInt(line[7]))
+                        Float.toString(Float.parseFloat(rec.get(2))),
+                        Float.toString(Float.parseFloat(rec.get(3))),
+                        Float.toString(Float.parseFloat(rec.get(4))),
+                        String.format("%.1f", 100.0f - Float.parseFloat(rec.get(5))),
+                        Integer.toString(Integer.parseInt(rec.get(7)))
                 );
             }
         });
 
         keyParserMap.put("MEM", new NMONItemParser() {
             @Override
-            public void parse(CSVPrinter writer, String[] line, Map<Integer, Long> timeTable) throws IOException {
-                Long ts = timeTable.get(Integer.parseInt(line[1].substring(1)));
+            public void parse(CSVPrinter writer, CSVRecord rec, Map<Integer, Long> timeTable) throws IOException {
+                Long ts = timeTable.get(Integer.parseInt(rec.get(1).substring(1)));
                 if (ts == null)
                     return;
                 writer.printRecord("MEM",
                         ts.toString(),
-                        Float.toString(Float.parseFloat(line[2])),
-                        Float.toString(Float.parseFloat(line[6])),
-                        Float.toString(Float.parseFloat(line[11])),
-                        Float.toString(Float.parseFloat(line[14]))
+                        Float.toString(Float.parseFloat(rec.get(2))),
+                        Float.toString(Float.parseFloat(rec.get(6))),
+                        Float.toString(Float.parseFloat(rec.get(11))),
+                        Float.toString(Float.parseFloat(rec.get(14)))
                 );
             }
         });
 
         keyParserMap.put("VM", new NMONItemParser() {
             @Override
-            public void parse(CSVPrinter writer, String[] line, Map<Integer, Long> timeTable) throws IOException {
-                Long ts = timeTable.get(Integer.parseInt(line[1].substring(1)));
+            public void parse(CSVPrinter writer, CSVRecord rec, Map<Integer, Long> timeTable) throws IOException {
+                Long ts = timeTable.get(Integer.parseInt(rec.get(1).substring(1)));
                 if (ts == null)
                     return;
                 writer.printRecord("VM",
                         ts.toString(),
-                        Float.toString(Float.parseFloat(line[10])),
-                        Float.toString(Float.parseFloat(line[11]))
+                        Float.toString(Float.parseFloat(rec.get(10))),
+                        Float.toString(Float.parseFloat(rec.get(11)))
                 );
             }
         });
 
         keyParserMap.put("NET", new NMONItemParser() {
             @Override
-            public void parse(CSVPrinter writer, String[] line, Map<Integer, Long> timeTable) throws IOException {
-                Long ts = timeTable.get(Integer.parseInt(line[1].substring(1)));
+            public void parse(CSVPrinter writer, CSVRecord rec, Map<Integer, Long> timeTable) throws IOException {
+                Long ts = timeTable.get(Integer.parseInt(rec.get(1).substring(1)));
                 if (ts == null)
                     return;
-                int numberOfNetworkAdapters = (line.length - 2) >> 1;
+                int numberOfNetworkAdapters = (rec.size() - 2) >> 1;
                 float read = 0;
                 float write = 0;
                 for (int i = 0; i < numberOfNetworkAdapters; ++i) {
-                    read += Float.parseFloat(line[2 + i]);
-                    write += Float.parseFloat(line[2 + numberOfNetworkAdapters + i]);
+                    read += Float.parseFloat(rec.get(2 + i));
+                    write += Float.parseFloat(rec.get(2 + numberOfNetworkAdapters + i));
                 }
                 writer.printRecord("NET",
                         ts.toString(),
@@ -134,8 +141,8 @@ public class NMONParser implements DataParser {
 
         keyParserMap.put("DISKBUSY", new NMONItemParser() {
             @Override
-            public void parse(CSVPrinter writer, String[] line, Map<Integer, Long> timeTable) throws IOException {
-                Long ts = timeTable.get(Integer.parseInt(line[1].substring(1)));
+            public void parse(CSVPrinter writer, CSVRecord rec, Map<Integer, Long> timeTable) throws IOException {
+                Long ts = timeTable.get(Integer.parseInt(rec.get(1).substring(1)));
                 if (ts == null)
                     return;
                 final String tsStr = ts.toString();
@@ -145,7 +152,7 @@ public class NMONParser implements DataParser {
                         continue;
                     writer.printRecord("DISKBUSY-" + disk,
                             tsStr,
-                            Float.toString(Float.parseFloat(line[index]))
+                            Float.toString(Float.parseFloat(rec.get(index)))
                     );
                 }
             }
@@ -153,8 +160,8 @@ public class NMONParser implements DataParser {
 
         keyParserMap.put("DISKREAD", new NMONItemParser() {
             @Override
-            public void parse(CSVPrinter writer, String[] line, Map<Integer, Long> timeTable) throws IOException {
-                Long ts = timeTable.get(Integer.parseInt(line[1].substring(1)));
+            public void parse(CSVPrinter writer, CSVRecord rec, Map<Integer, Long> timeTable) throws IOException {
+                Long ts = timeTable.get(Integer.parseInt(rec.get(1).substring(1)));
                 if (ts == null)
                     return;
                 final String tsStr = ts.toString();
@@ -164,7 +171,7 @@ public class NMONParser implements DataParser {
                         continue;
                     writer.printRecord("DISKREAD-" + disk,
                             tsStr,
-                            Float.toString(Float.parseFloat(line[index]))
+                            Float.toString(Float.parseFloat(rec.get(index)))
                     );
                 }
             }
@@ -172,8 +179,8 @@ public class NMONParser implements DataParser {
 
         keyParserMap.put("DISKWRITE", new NMONItemParser() {
             @Override
-            public void parse(CSVPrinter writer, String[] line, Map<Integer, Long> timeTable) throws IOException {
-                Long ts = timeTable.get(Integer.parseInt(line[1].substring(1)));
+            public void parse(CSVPrinter writer, CSVRecord rec, Map<Integer, Long> timeTable) throws IOException {
+                Long ts = timeTable.get(Integer.parseInt(rec.get(1).substring(1)));
                 if (ts == null)
                     return;
                 final String tsStr = ts.toString();
@@ -183,7 +190,7 @@ public class NMONParser implements DataParser {
                         continue;
                     writer.printRecord("DISKWRITE-" + disk,
                             tsStr,
-                            Float.toString(Float.parseFloat(line[index]))
+                            Float.toString(Float.parseFloat(rec.get(index)))
                     );
                 }
             }
@@ -191,18 +198,18 @@ public class NMONParser implements DataParser {
 
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
         CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT);
-        for (String[] line : lines) {
-            if (line.length < 2 || !line[1].startsWith("T"))
+        for (CSVRecord rec : records) {
+            if (rec.size() < 2 || !rec.get(1).startsWith("T"))
                 continue;
-            NMONItemParser nmonItemParser = keyParserMap.get(line[0]);
-            if (nmonItemParser != null)
-                nmonItemParser.parse(csvPrinter, line, timeTable);
+            NMONItemParser itemParser = keyParserMap.get(rec.get(0));
+            if (itemParser != null)
+                itemParser.parse(csvPrinter, rec, timeTable);
         }
         csvPrinter.flush();
     }
 
     private interface NMONItemParser {
-        void parse(CSVPrinter writer, String[] line, Map<Integer, Long> timeTable) throws IOException,
+        void parse(CSVPrinter writer, CSVRecord rec, Map<Integer, Long> timeTable) throws IOException,
                 ParseException;
     }
 }
